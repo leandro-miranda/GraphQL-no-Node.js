3c64b98ba35ce8ea725493a2e3969ca7aa56fdb9
leandro-miranda <leandromiranda2010@gmail.com>
2023-03-14 19:29:48 -0300

    Criando 2 apps completos com GraphQL no Node.js

---
+++ src/resolvers/appointments-resolver.ts
@@ -0,0 +1,34 @@
+import { Appointment } from './../dtos/models/appointment-model';
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from 'type-graphql';
+import { CreateAppointmentInput } from '../dtos/inputs/create-appointment-input';
+import { Customer } from '../dtos/models/customer-model';
+
+@Resolver(() => Appointment)
+export class AppointmentsResolver  {
+	@Query(() => [Appointment])
+	async appointments() {
+		return [
+			{
+				startsAt: new Date(),
+				endsAt: new Date(),
+			}
+		];
+	}
+
+	@Mutation(() => Appointment)
+	async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+		const appointment = {
+			startsAt: data.startsAt,
+			endsAt: data.endsAt,
+		}
+
+		return appointment;
+	}
+
+	@FieldResolver(() => Customer)
+	async customer(@Root() appointment: Appointment) {
+		return {
+			name: 'John Doe',
+		}
+	}
+}
